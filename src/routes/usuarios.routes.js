a149aef3921e9873148edaf366eec54b9677a6f7
João Vitor <joaovitorportosales46@gmail.com>
2024-11-07 15:58:50 -0300

    Feat: add validation

--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -20,6 +20,19 @@ carrosRoutes.post("/", (req, res) => {
     const { modeloVeiculo, quilometragem, status, problemasReportados } = req.body;
 
     const carro = carrosList.addCarros( modeloVeiculo, quilometragem, status, problemasReportados);
+
+    if(!modeloVeiculo || !quilometragem ){
+      return res.status(400).json({
+        message: "Os campos de modelo veículo e quilometragem são obrigatórios"
+      })
+      
+    }
+
+    if(status != "pendente" && status != "manutenção" && status != "finalizado"){
+      return res.status(400).json({
+        message: "O campo status deve ser preenchido com pendente, manutenção ou finalizado."
+      })
+    }
   
     return res.status(201).json({
       message: "Carro cadastrado com sucesso!",
